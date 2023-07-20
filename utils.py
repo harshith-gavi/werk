7a0ad3fe734c2ad2d2ad45c4c2802f15aa8ca9d6
harshith-gavi <91324439+harshith-gavi@users.noreply.github.com>
2023-07-20 12:58:35 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -28,7 +28,7 @@ def update_prob_estimates( model, args, train_loader, estimatedDistribution, est
     for batch_idx, (data, target) in enumerate(train_loader):
         if args.cuda: data, target = data.cuda(), target.cuda()
         # data = data.view(-1, input_channels, seq_length)
-         data = data.to_dense()
+        data = data.to_dense()
         
         B = target.size()[0]
         step = model.network[0].step
